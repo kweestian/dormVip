8fec6e5fc2764b07b8f98ecc44b5ba074300ec6a
kweestian <chrislhamelin@gmail.com>
2015-08-25 21:12:03 -0400

    SO hack again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,10 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = true
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
