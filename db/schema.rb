3e95736ec561bf64794022f56b7493f4e14d8667
kweestian <chrislhamelin@gmail.com>
2015-08-26 23:41:27 -0400

    betterish ui

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814232233) do
+ActiveRecord::Schema.define(version: 20150827023229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,5 +39,12 @@ ActiveRecord::Schema.define(version: 20150814232233) do
   end
 
   add_index "products", ["bundle_id"], name: "index_products_on_bundle_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
