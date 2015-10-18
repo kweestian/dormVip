2dd691a3f7b3727851af31f93c9032ffa3b9ce03
Christian <chrislhamelin@gmail.com>
2015-10-18 19:31:30 -0400

    controller to etc

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,10 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:edit, :update, :destroy]
 
+  # todo set up relations instead of nth_item
+
   def new
-    @order = Order.new
+    @order = current_user.orders.new
     @asked_frequency = params[:asked_frequency]
     @frequencies = ["Once", "For a Month", "Semester",  "For a Year"]
   end
@@ -11,7 +13,7 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(order_params)
+    @order = current_user.orders.new(order_params)
 
     respond_to do |format|
       if @order.save
@@ -49,11 +51,12 @@ class OrdersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
-      @order = Order.find(params[:id])
+      @order = Order.where(user_id = current_user.id)
     end
 
-  def order_params
-      params.require(:order).permit(:first_item, :second_item, :third_item, :user_id, :frequency, order_items_attributes: [ :id, :product_id])
-  end
+    def order_params
+
+        params.require(:order).permit(:first_item, :second_item, :third_item, :user_id, :frequency, order_items_attributes: [ :id, :product_id])
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
+      if @user.update(user_params_edit)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
@@ -70,5 +70,9 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+    def user_params_edit
+      params.require(:user).permit(:first_name, :last_name, :email)
     end
 end
